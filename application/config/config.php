1ffa7b13dd616e875e5e11e8a0c102718a87b022
Shen Yichen <2007.yichen@gmail.com>
2016-09-07 01:46:35 +0800

    Set up connector for postgres
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- application/config/config.php
+++ application/config/config.php
@@ -53,9 +53,8 @@ define('URL', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER);
  * Configuration for: Database
  * This is the place where you define your database credentials, database type etc.
  */
-define('DB_TYPE', 'mysql');
+define('DB_TYPE', 'pgsql');
 define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'mini');
-define('DB_USER', 'root');
-define('DB_PASS', '12345678');
-define('DB_CHARSET', 'utf8');
+define('DB_NAME', 'cs2102');
+define('DB_USER', 'postgres');
+define('DB_PASS', '');
