5f0be7890489d094063330eca84378cfefcb4948
Nguyen Quoc Bao <ryuu.sakamoto.164@outlook.com>
2016-09-25 23:57:28 +0800

    Added end date column for projects

---
+++ _install/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (email, name) VALUES ('abc@example.com', 'abc');
+
+INSERT INTO projects (owner, title, start_date, duration, amount) VALUES
+('abc@example.com', 'test project', '2008-01-01', 20, 100);
--- _install/schema.sql
+++ _install/schema.sql
@@ -1,23 +1,42 @@
+CREATE TABLE users (
+  email VARCHAR(128) PRIMARY KEY,
+  name VARCHAR(128) NOT NULL
+);
+
 CREATE TABLE projects (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   owner VARCHAR(128) REFERENCES users(email),
   title text NOT NULL,
   description text,
   start_date DATE NOT NULL,
+  end_date DATE,
   duration INT CHECK (duration > 0),
   categories text,
-  amount INT NOT NULL CHECK (amount > 0),
-  curr_amount INT DEFAULT 0
-);
-
-CREATE TABLE users (
-  email VARCHAR(128) PRIMARY KEY,
-  name VARCHAR(128) NOT NULL,
+  amount FLOAT NOT NULL CHECK (amount > 0),
+  curr_amount FLOAT DEFAULT 0
 );
 
 CREATE TABLE fundings (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   p_id INT REFERENCES projects(id),
   funder VARCHAR(128) REFERENCES users(email),
-  amount INT
-)
+  amount FLOAT
+);
+
+CREATE OR REPLACE FUNCTION end_date_insert()
+  RETURNS trigger AS
+$$
+BEGIN
+    UPDATE projects
+    SET end_date = start_date + duration where id = new.id;
+
+    RETURN NULL;
+END;
+$$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER AddEndDate
+AFTER INSERT
+ON projects
+FOR EACH ROW
+EXECUTE PROCEDURE end_date_insert();
