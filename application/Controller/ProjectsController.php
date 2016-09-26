7c798ed5b81282b4cf40db72740295e6461bae48
Shen Yichen <2007.yichen@gmail.com>
2016-09-26 19:40:17 +0800

    Fix indentation
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- application/Controller/ProjectsController.php
+++ application/Controller/ProjectsController.php
@@ -6,130 +6,138 @@ use Mini\Model\Project;
 
 class ProjectsController
 {
-    /**
-     * PAGE: index
-     * This method handles what happens when you move to http://yourproject/songs/index
-     */
-    public function index() {
-        // Instance new Model (Song)
-        $Project = new Project();
-        // getting all songs and amount of songs
-        $projects = $Project->getAllProjects();
+  /**
+   * PAGE: index
+   * This method handles what happens when you move to http://yourproject/songs/index
+   */
+  public function index()
+  {
+    // Instance new Model (Song)
+    $Project = new Project();
+    // getting all songs and amount of songs
+    $projects = $Project->getAllProjects();
 
-        // load views. within the views we can echo out $songs and $amount_of_songs easily
-        include APP . 'view/_templates/header.php';
-        include APP . 'view/projects/index.php';
-        include APP . 'view/_templates/footer.php';
+    // load views. within the views we can echo out $songs and $amount_of_songs easily
+    include APP . 'view/_templates/header.php';
+    include APP . 'view/projects/index.php';
+    include APP . 'view/_templates/footer.php';
+  }
+
+  public function create()
+  {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    }
+  }
+
+  /**
+   * ACTION: addSong
+   * This method handles what happens when you move to http://yourproject/songs/addsong
+   * IMPORTANT: This is not a normal page, it's an ACTION. This is where the "add a song" form on songs/index
+   * directs the user after the form submit. This method handles all the POST data from the form and then redirects
+   * the user back to songs/index via the last line: header(...)
+   * This is an example of how to handle a POST request.
+   */
+  public function addSong()
+  {
+    // if we have POST data to create a new song entry
+    if (isset($_POST["submit_add_song"])) {
+      // Instance new Model (Song)
+      $Song = new Song();
+      // do addSong() in model/model.php
+      $Song->addSong($_POST["artist"], $_POST["track"], $_POST["link"]);
     }
 
-    /**
-     * ACTION: addSong
-     * This method handles what happens when you move to http://yourproject/songs/addsong
-     * IMPORTANT: This is not a normal page, it's an ACTION. This is where the "add a song" form on songs/index
-     * directs the user after the form submit. This method handles all the POST data from the form and then redirects
-     * the user back to songs/index via the last line: header(...)
-     * This is an example of how to handle a POST request.
-     */
-    public function addSong()
-    {
-        // if we have POST data to create a new song entry
-        if (isset($_POST["submit_add_song"])) {
-            // Instance new Model (Song)
-            $Song = new Song();
-            // do addSong() in model/model.php
-            $Song->addSong($_POST["artist"], $_POST["track"],  $_POST["link"]);
-        }
+    // where to go after song has been added
+    header('location: ' . URL . 'songs/index');
+  }
 
-        // where to go after song has been added
-        header('location: ' . URL . 'songs/index');
+  /**
+   * ACTION: deleteSong
+   * This method handles what happens when you move to http://yourproject/songs/deletesong
+   * IMPORTANT: This is not a normal page, it's an ACTION. This is where the "delete a song" button on songs/index
+   * directs the user after the click. This method handles all the data from the GET request (in the URL!) and then
+   * redirects the user back to songs/index via the last line: header(...)
+   * This is an example of how to handle a GET request.
+   *
+   * @param int $song_id Id of the to-delete song
+   */
+  public function deleteSong($song_id)
+  {
+    // if we have an id of a song that should be deleted
+    if (isset($song_id)) {
+      // Instance new Model (Song)
+      $Song = new Song();
+      // do deleteSong() in model/model.php
+      $Song->deleteSong($song_id);
     }
 
-    /**
-     * ACTION: deleteSong
-     * This method handles what happens when you move to http://yourproject/songs/deletesong
-     * IMPORTANT: This is not a normal page, it's an ACTION. This is where the "delete a song" button on songs/index
-     * directs the user after the click. This method handles all the data from the GET request (in the URL!) and then
-     * redirects the user back to songs/index via the last line: header(...)
-     * This is an example of how to handle a GET request.
-     *
-     * @param int $song_id Id of the to-delete song
-     */
-    public function deleteSong($song_id)
-    {
-        // if we have an id of a song that should be deleted
-        if (isset($song_id)) {
-            // Instance new Model (Song)
-            $Song = new Song();
-            // do deleteSong() in model/model.php
-            $Song->deleteSong($song_id);
-        }
+    // where to go after song has been deleted
+    header('location: ' . URL . 'songs/index');
+  }
 
-        // where to go after song has been deleted
-        header('location: ' . URL . 'songs/index');
+  /**
+   * ACTION: editSong
+   * This method handles what happens when you move to http://yourproject/songs/editsong
+   *
+   * @param int $song_id Id of the to-edit song
+   */
+  public function editSong($song_id)
+  {
+    // if we have an id of a song that should be edited
+    if (isset($song_id)) {
+      // Instance new Model (Song)
+      $Song = new Song();
+      // do getSong() in model/model.php
+      $song = $Song->getSong($song_id);
+
+      // in a real application we would also check if this db entry exists and therefore show the result or
+      // redirect the user to an error page or similar
+
+      // load views. within the views we can echo out $song easily
+      include APP . 'view/_templates/header.php';
+      include APP . 'view/songs/edit.php';
+      include APP . 'view/_templates/footer.php';
+    } else {
+      // redirect user to songs index page (as we don't have a song_id)
+      header('location: ' . URL . 'songs/index');
+    }
+  }
+
+  /**
+   * ACTION: updateSong
+   * This method handles what happens when you move to http://yourproject/songs/updatesong
+   * IMPORTANT: This is not a normal page, it's an ACTION. This is where the "update a song" form on songs/edit
+   * directs the user after the form submit. This method handles all the POST data from the form and then redirects
+   * the user back to songs/index via the last line: header(...)
+   * This is an example of how to handle a POST request.
+   */
+  public function updateSong()
+  {
+    // if we have POST data to create a new song entry
+    if (isset($_POST["submit_update_song"])) {
+      // Instance new Model (Song)
+      $Song = new Song();
+      // do updateSong() from model/model.php
+      $Song->updateSong($_POST["artist"], $_POST["track"], $_POST["link"], $_POST['song_id']);
     }
 
-     /**
-     * ACTION: editSong
-     * This method handles what happens when you move to http://yourproject/songs/editsong
-      *
-     * @param int $song_id Id of the to-edit song
-     */
-    public function editSong($song_id)
-    {
-        // if we have an id of a song that should be edited
-        if (isset($song_id)) {
-            // Instance new Model (Song)
-            $Song = new Song();
-            // do getSong() in model/model.php
-            $song = $Song->getSong($song_id);
+    // where to go after song has been added
+    header('location: ' . URL . 'songs/index');
+  }
 
-            // in a real application we would also check if this db entry exists and therefore show the result or
-            // redirect the user to an error page or similar
+  /**
+   * AJAX-ACTION: ajaxGetStats
+   * TODO documentation
+   */
+  public function ajaxGetStats()
+  {
+    // Instance new Model (Song)
+    $Song = new Song();
+    $amount_of_songs = $Song->getAmountOfSongs();
 
-            // load views. within the views we can echo out $song easily
-            include APP . 'view/_templates/header.php';
-            include APP . 'view/songs/edit.php';
-            include APP . 'view/_templates/footer.php';
-        } else {
-            // redirect user to songs index page (as we don't have a song_id)
-            header('location: ' . URL . 'songs/index');
-        }
-    }
-
-    /**
-     * ACTION: updateSong
-     * This method handles what happens when you move to http://yourproject/songs/updatesong
-     * IMPORTANT: This is not a normal page, it's an ACTION. This is where the "update a song" form on songs/edit
-     * directs the user after the form submit. This method handles all the POST data from the form and then redirects
-     * the user back to songs/index via the last line: header(...)
-     * This is an example of how to handle a POST request.
-     */
-    public function updateSong()
-    {
-        // if we have POST data to create a new song entry
-        if (isset($_POST["submit_update_song"])) {
-            // Instance new Model (Song)
-            $Song = new Song();
-            // do updateSong() from model/model.php
-            $Song->updateSong($_POST["artist"], $_POST["track"],  $_POST["link"], $_POST['song_id']);
-        }
-
-        // where to go after song has been added
-        header('location: ' . URL . 'songs/index');
-    }
-
-    /**
-     * AJAX-ACTION: ajaxGetStats
-     * TODO documentation
-     */
-    public function ajaxGetStats()
-    {
-        // Instance new Model (Song)
-        $Song = new Song();
-        $amount_of_songs = $Song->getAmountOfSongs();
-
-        // simply echo out something. A supersimple API would be possible by echoing JSON here
-        echo $amount_of_songs;
-    }
+    // simply echo out something. A supersimple API would be possible by echoing JSON here
+    echo $amount_of_songs;
+  }
 
 }
