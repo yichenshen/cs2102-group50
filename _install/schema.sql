cb33fd69cea4b6b1f5cc3b66b220e78124d19665
Shen Yichen <2007.yichen@gmail.com>
2016-09-26 16:01:42 +0800

    Modify schema
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- _install/schema.sql
+++ _install/schema.sql
@@ -10,33 +10,13 @@ CREATE TABLE projects (
   description text,
   start_date DATE NOT NULL,
   end_date DATE,
-  duration INT CHECK (duration > 0),
   categories text,
-  amount FLOAT NOT NULL CHECK (amount > 0),
-  curr_amount FLOAT DEFAULT 0
+  amount FLOAT NOT NULL CHECK (amount > 0)
 );
 
 CREATE TABLE fundings (
   id SERIAL PRIMARY KEY,
   p_id INT REFERENCES projects(id),
   funder VARCHAR(128) REFERENCES users(email),
-  amount FLOAT
+  amount FLOAT NOT NULL CHECK (amount > 0)
 );
-
-CREATE OR REPLACE FUNCTION end_date_insert()
-  RETURNS trigger AS
-$$
-BEGIN
-    UPDATE projects
-    SET end_date = start_date + duration where id = new.id;
-
-    RETURN NULL;
-END;
-$$
-LANGUAGE 'plpgsql';
-
-CREATE TRIGGER AddEndDate
-AFTER INSERT
-ON projects
-FOR EACH ROW
-EXECUTE PROCEDURE end_date_insert();
