86ff9dcde0390b49ac60bb4b700509096a5d6a04
Shen Yichen <2007.yichen@gmail.com>
2016-09-26 17:06:00 +0800

    Linting
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- application/Controller/ProjectsController.php
+++ application/Controller/ProjectsController.php
@@ -10,17 +10,16 @@ class ProjectsController
      * PAGE: index
      * This method handles what happens when you move to http://yourproject/songs/index
      */
-    public function index()
-    {
+    public function index() {
         // Instance new Model (Song)
         $Project = new Project();
         // getting all songs and amount of songs
         $projects = $Project->getAllProjects();
 
-       // load views. within the views we can echo out $songs and $amount_of_songs easily
-        require APP . 'view/_templates/header.php';
+        // load views. within the views we can echo out $songs and $amount_of_songs easily
+        include APP . 'view/_templates/header.php';
         print_r($projects);
-        require APP . 'view/_templates/footer.php';
+        include APP . 'view/_templates/footer.php';
     }
 
     /**
@@ -52,6 +51,7 @@ class ProjectsController
      * directs the user after the click. This method handles all the data from the GET request (in the URL!) and then
      * redirects the user back to songs/index via the last line: header(...)
      * This is an example of how to handle a GET request.
+     *
      * @param int $song_id Id of the to-delete song
      */
     public function deleteSong($song_id)
@@ -71,6 +71,7 @@ class ProjectsController
      /**
      * ACTION: editSong
      * This method handles what happens when you move to http://yourproject/songs/editsong
+      *
      * @param int $song_id Id of the to-edit song
      */
     public function editSong($song_id)
@@ -86,9 +87,9 @@ class ProjectsController
             // redirect the user to an error page or similar
 
             // load views. within the views we can echo out $song easily
-            require APP . 'view/_templates/header.php';
-            require APP . 'view/songs/edit.php';
-            require APP . 'view/_templates/footer.php';
+            include APP . 'view/_templates/header.php';
+            include APP . 'view/songs/edit.php';
+            include APP . 'view/_templates/footer.php';
         } else {
             // redirect user to songs index page (as we don't have a song_id)
             header('location: ' . URL . 'songs/index');
