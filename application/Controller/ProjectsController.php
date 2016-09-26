e8bfa29cd88bf301a77fb003150e763e3840d053
Shen Yichen <2007.yichen@gmail.com>
2016-09-26 20:39:28 +0800

    Add delete controller
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- application/Controller/ProjectsController.php
+++ application/Controller/ProjectsController.php
@@ -23,6 +23,7 @@ class ProjectsController
     include APP . 'view/_templates/footer.php';
   }
 
+  // POST
   public function create()
   {
     if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
@@ -43,52 +44,20 @@ class ProjectsController
 //    header('Location:' . URL . 'projects/show/' . $newID);
   }
 
-  /**
-   * ACTION: addSong
-   * This method handles what happens when you move to http://yourproject/songs/addsong
-   * IMPORTANT: This is not a normal page, it's an ACTION. This is where the "add a song" form on songs/index
-   * directs the user after the form submit. This method handles all the POST data from the form and then redirects
-   * the user back to songs/index via the last line: header(...)
-   * This is an example of how to handle a POST request.
-   */
-  public
-  function addSong()
+  // POST
+  public function deleteProject($projectId)
   {
-    // if we have POST data to create a new song entry
-    if (isset($_POST["submit_add_song"])) {
-      // Instance new Model (Song)
-      $Song = new Song();
-      // do addSong() in model/model.php
-      $Song->addSong($_POST["artist"], $_POST["track"], $_POST["link"]);
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+      header('Location:' . URL . 'error');
+      return;
     }
 
-    // where to go after song has been added
-    header('location: ' . URL . 'songs/index');
-  }
-
-  /**
-   * ACTION: deleteSong
-   * This method handles what happens when you move to http://yourproject/songs/deletesong
-   * IMPORTANT: This is not a normal page, it's an ACTION. This is where the "delete a song" button on songs/index
-   * directs the user after the click. This method handles all the data from the GET request (in the URL!) and then
-   * redirects the user back to songs/index via the last line: header(...)
-   * This is an example of how to handle a GET request.
-   *
-   * @param int $song_id Id of the to-delete song
-   */
-  public
-  function deleteSong($song_id)
-  {
-    // if we have an id of a song that should be deleted
-    if (isset($song_id)) {
-      // Instance new Model (Song)
-      $Song = new Song();
-      // do deleteSong() in model/model.php
-      $Song->deleteSong($song_id);
+    if (isset($projectId)) {
+      $Project = new Project();
+      $Project->deleteProject($projectId);
     }
 
-    // where to go after song has been deleted
-    header('location: ' . URL . 'songs/index');
+    header('location: ' . URL . 'projects');
   }
 
   /**
