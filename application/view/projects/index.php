e6c01ec4989871ae046bd1216284341d5ca543f2
Shen Yichen <2007.yichen@gmail.com>
2016-09-26 19:18:44 +0800

    Add more attributes to projects index
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- application/view/projects/index.php
+++ application/view/projects/index.php
@@ -5,20 +5,24 @@
   <div class="box">
     <table>
       <thead>
-        <tr>
-          <td>Title</td>
-          <td>Description</td>
-          <td>Start Date</td>
-        </tr>
+      <tr>
+        <td>Title</td>
+        <td>Description</td>
+        <td>Start Date</td>
+        <td>End Date</td>
+        <td>Amount Needed</td>
+      </tr>
       </thead>
       <tbody>
-        <?php foreach ($projects as $project) { ?>
-          <tr>
-            <td><?php echo htmlspecialchars($project->title) ?></td>
-            <td><?php echo htmlspecialchars($project->description)?></td>
-            <td><?php echo htmlspecialchars($project->start_date)?></td>
-          </tr>
-        <?php } ?>
+      <?php foreach ($projects as $project) { ?>
+        <tr>
+          <td><?php echo htmlspecialchars($project->title) ?></td>
+          <td><?php echo htmlspecialchars($project->description) ?></td>
+          <td><?php echo htmlspecialchars($project->start_date) ?></td>
+          <td><?php echo htmlspecialchars($project->end_date) ?></td>
+          <td><?php echo htmlspecialchars($project->amount) ?></td>
+        </tr>
+      <?php } ?>
       </tbody>
     </table>
   </div>
