5dfcb64f7a1dd274e5ec3d72d6317ede57eb43ad
Shen Yichen <2007.yichen@gmail.com>
2016-09-28 01:01:14 +0800

    Add a new project form
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

---
+++ application/view/projects/_form.php
@@ -0,0 +1,12 @@
+<form action="/projects/create">
+  <div class="row">
+    <div class="input-field col s12">
+      <input id="title" name="title" type="text" class="validate">
+      <label for="title">Project Title</label>
+    </div>
+    <div class="input-field col s12">
+      <textarea id="description" name="description" class="materialize-textarea"></textarea>
+      <label for="description">Description</label>
+    </div>
+  </div>
+</form>
---
+++ application/view/projects/new.php
@@ -0,0 +1,4 @@
+<div class="container">
+  <h1>New Project</h1>
+  <?php include '_form.php' ?>
+</div>
