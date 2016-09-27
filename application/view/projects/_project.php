99c1646ecb3e3ea531f7c688ceceaab278458a5d
Shen Yichen <2007.yichen@gmail.com>
2016-09-28 00:51:11 +0800

    Abstract out a partial
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

---
+++ application/view/projects/_project.php
@@ -0,0 +1,18 @@
+<tr>
+  <td><?php echo htmlspecialchars($project->title) ?></td>
+  <td><?php echo htmlspecialchars($project->description) ?></td>
+  <td><?php echo htmlspecialchars($project->start_date) ?></td>
+  <td><?php echo htmlspecialchars($project->end_date) ?></td>
+  <td><?php echo htmlspecialchars($project->amount) ?></td>
+  <td>
+    <form action="/projects/delete/<?php echo $project->id ?>" method="post">
+      <a class="btn-floating green lighten-1" href="/projects/edit/<?php echo $project->id ?>">
+        <i class="material-icons">edit</i>
+      </a>
+      <button class="btn-floating red accent-2" type="submit" name="action">
+        <i class="material-icons">delete</i>
+      </button>
+    </form>
+  </td>
+</tr>
+
