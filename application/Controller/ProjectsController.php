dcc6da4946b4287db7616223b9740dda09a83768
Shen Yichen <2007.yichen@gmail.com>
2016-09-28 01:58:52 +0800

    Add edit page
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- application/Controller/ProjectsController.php
+++ application/Controller/ProjectsController.php
@@ -31,6 +31,15 @@ class ProjectsController
     include APP . 'view/_templates/footer.php';
   }
 
+  public function edit($projectId){
+    $Project = new Project();
+    $project = $Project->getProject($projectId);
+
+    include APP . 'view/_templates/header.php';
+    include APP . 'view/projects/edit.php';
+    include APP . 'view/_templates/footer.php';
+  }
+
   // POST
   public function create()
   {
@@ -49,6 +58,27 @@ class ProjectsController
 
     //TODO: fill up empty fields.
     $newID = $Project->addProject(null, $title, $description, $startDate, $endDate, null, $amount);
+
+    header('Location:' . URL . 'projects/');
+  }
+
+  public function update($projectID)
+  {
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+      header('Location:' . URL . 'error');
+      return;
+    }
+
+    $Project = new Project();
+
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $startDate = $_POST['start_date'];
+    $endDate = $_POST['end_date'];
+    $amount = $_POST['amount'];
+
+    //TODO: fill up empty fields.
+    $newID = $Project->updateProject(null, $title, $description, $startDate, $endDate, null, $amount, $projectID);
 
     header('Location:' . URL . 'projects/');
   }
---
+++ application/view/projects/edit.php
@@ -0,0 +1,5 @@
+<div class="container">
+  <h1><?php echo $project->title ?></h1>
+  <?php $url = '/projects/update' ?>
+  <?php include '_form.php' ?>
+</div>
