3a6214ef9b8a63532fb73ec5a49ed3c69ca7371d
Shen Yichen <2007.yichen@gmail.com>
2016-09-28 02:03:59 +0800

    Add edit view
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- application/view/projects/edit.php
+++ application/view/projects/edit.php
@@ -1,5 +1,5 @@
 <div class="container">
   <h1><?php echo $project->title ?></h1>
-  <?php $url = '/projects/update' ?>
+  <?php $url = '/projects/update/' . $project->id  ?>
   <?php include '_form.php' ?>
 </div>
