5f83bf450fa0cee98ed05a5c4cfbd50a7caa646b
Shen Yichen <2007.yichen@gmail.com>
2016-09-28 00:14:52 +0800

    Add header and footer structure
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -1,3 +1,9 @@
+</main>
+
+<footer>
+
+</footer>
+
 <!-- jQuery, loaded in the recommended protocol-less way -->
 <!-- more http://www.paulirish.com/2010/the-protocol-relative-url/ -->
 <script src="<?php echo URL; ?>bower_components/jquery/dist/jquery.min.js"></script>
--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -23,15 +23,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
-<nav class="light-blue lighten-1" role="navigation">
-  <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
-    <ul class="right hide-on-med-and-down">
-      <li><a href="#">Navbar Link</a></li>
-    </ul>
+<header>
+  <nav class="light-blue lighten-1" role="navigation">
+    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">Navbar Link</a></li>
+      </ul>
 
-    <ul id="nav-mobile" class="side-nav">
-      <li><a href="#">Navbar Link</a></li>
-    </ul>
-    <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
-  </div>
-</nav>
+      <ul id="nav-mobile" class="side-nav">
+        <li><a href="#">Navbar Link</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+</header>
+
+<main>
