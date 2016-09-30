b7623b3170a48a409ee492db38a592b49d523fab
Shen Yichen <2007.yichen@gmail.com>
2016-09-30 20:37:54 +0800

    Tweaks to login
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- application/view/login/index.php
+++ application/view/login/index.php
@@ -5,30 +5,36 @@
       <form action="/login/login" method="post">
         <div class="card">
           <div class="card-image primary-700 darken-2">
-            <img src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" height="140" alt=""/>
+            <img src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" height="130"/>
             <span class="card-title"><h3>Welcome back!</h3></span>
           </div>
           <div class="card-content">
             <div class="row">
-              <div class="input-field col s12">
+              <div class="input-field col l10 offset-l1 m12">
                 <input id="email" name="email" type="text" class="validate">
                 <label for="email">Email</label>
               </div>
-              <div class="input-field col s12">
+              <div class="input-field col l10 offset-l1 m12">
                 <input id="password" name="password" type="password" class="validate">
                 <label for="password">Password</label>
               </div>
-              <div class="input-field col s12">
+              <div class="input-field col l10 offset-l1 m12">
                 <input type="checkbox" id="remember" name="remember">
                 <label for="remember">Remember Me</label>
               </div>
-              <button class="btn btn-floating btn-large secondary-accent right">
-                <i class="material-icons">send</i>
+            </div>
+            <br />
+            <div class="card-action">
+              <a class="btn btn-flat btn-large left blue-text text-darken-1">Sign Up</a>
+              <button class="btn btn-floating btn-large waves-effect waves-light secondary-accent right" type="submit" name="action">
+                  <i class="material-icons right">send</i>
               </button>
+              <br />
             </div>
           </div>
         </div>
       </form>
     </div>
+    <div
   </div>
 </div>
