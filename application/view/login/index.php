850010fd30d31a6b355751e59a52f2cd295386a1
Shen Yichen <2007.yichen@gmail.com>
2016-09-30 19:39:05 +0800

    Add login card
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- application/view/login/index.php
+++ application/view/login/index.php
@@ -1,19 +1,31 @@
 <div class="container">
-  <h1>Login</h1>
-  <form action="/login/login" method="post">
-    <div class="row">
-      <div class="input-field col s12">
-        <input id="email" name="email" type="text" class="validate">
-        <label for="email">Email</label>
-      </div>
-      <div class="input-field col s12">
-        <input id="password" name="password" type="password" class="validate">
-        <label for="password">Password</label>
+  <br/>
+  <div class="row">
+    <div class="col l8 offset-l2 m12">
+      <div class="card">
+        <div class="card-image main-700 darken-2">
+          <img src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" height="140" alt=""/>
+          <span class="card-title"><h3>Welcome back!</h3></span>
+        </div>
+        <div class="card-content">
+          <form action="/login/login" method="post">
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="email" name="email" type="text" class="validate">
+                <label for="email">Email</label>
+              </div>
+              <div class="input-field col s12">
+                <input id="password" name="password" type="password" class="validate">
+                <label for="password">Password</label>
+              </div>
+              <div class="input-field col s12">
+                <input type="checkbox" id="remember" name="remember">
+                <label for="remember">Remember Me</label>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
-    <div class="input-field col s12">
-      <input type="checkbox" id="remember" name="remember">
-      <label for="remember">Remember Me</label>
-    </div>
-  </form>
+  </div>
 </div>
