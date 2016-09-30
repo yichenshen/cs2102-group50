069f92187fb98edc564fa9eafaba59182941306e
Shen Yichen <2007.yichen@gmail.com>
2016-09-30 18:33:38 +0800

    Add login screen
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- application/view/login/index.php
+++ application/view/login/index.php
@@ -1,7 +1,19 @@
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: yichen
- * Date: 9/30/16
- * Time: 6:04 PM
- */
+<div class="container">
+  <h1>Login</h1>
+  <form action="/login/login" method="post">
+    <div class="row">
+      <div class="input-field col s12">
+        <input id="email" name="email" type="text" class="validate">
+        <label for="email">Email</label>
+      </div>
+      <div class="input-field col s12">
+        <input id="password" name="password" type="password" class="validate">
+        <label for="password">Password</label>
+      </div>
+    </div>
+    <div class="input-field col s12">
+      <input type="checkbox" id="remember" name="remember">
+      <label for="remember">Remember Me</label>
+    </div>
+  </form>
+</div>
