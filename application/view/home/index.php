5d4a7dc7c9db037feb229f3217e20b1b98599a15
Shen Yichen <2007.yichen@gmail.com>
2016-10-01 18:30:56 +0800

    Main page
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -1,5 +1,47 @@
+<div class="section no-pad-bot primary-300">
+  <br><br>
+  <h1 class="header center orange-text text-darken-3"><b>KACHING!</b></h1>
+  <div class="center">
+    <h4 class="header light">The more, the merrier!</h4>
+    <h5 class="header light">Crowd Fund your project today</h5>
+    <br/>
+  </div>
+  <div class="center">
+    <a href=""
+       class="btn-large waves-effect waves-light secondary-accent">Sign Up now</a>
+  </div>
+  <br><br>
+</div>
 <div class="container">
-    <h1>Home</h1>
-    <h2>You are in the View: application/view/home/index.php (everything in the box comes from this file)</h2>
-    <p>In a real application this could be the homepage.</p>
+  <div class="row">
+    <div class="col s12 m4">
+      <h2 class="center amber-text text-darken-3"><i class="material-icons large">lightbulb_outline</i></h2>
+      <h5 class="center">Realise your dream</h5>
+
+      <p class="light">Aliquam finibus, urna at convallis ultrices, lacus magna sodales metus,
+        at molestie augue urna nec sapien. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+        turpis egestas. Sed ornare urna ex. Ut dignissim, mauris nec efficitur commodo, dui enim elementum ante, ut
+        iaculis purus augue quis turpis.</p>
+    </div>
+
+    <div class="col s12 m4">
+      <h2 class="center blue-text"><i class="material-icons large">trending_up</i></h2>
+      <h5 class="center">Grow your project</h5>
+
+      <p class="light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras condimentum, lorem sit amet semper
+        faucibus, risus felis bibendum dolor, tincidunt congue ipsum eros nec nulla. Duis risus quam, pharetra porta
+        iaculis posuere, fringilla iaculis orci.</p>
+    </div>
+
+    <div class="col s12 m4">
+      <div class="icon-block">
+        <h2 class="center green-text"><i class="material-icons large">track_changes</i></h2>
+        <h5 class="center">Moniter your progress</h5>
+
+        <p class="light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras condimentum, lorem sit amet semper
+          faucibus, risus felis bibendum dolor, tincidunt congue ipsum eros nec nulla. Duis risus quam, pharetra porta
+          iaculis posuere, fringilla iaculis orci</p>
+      </div>
+    </div>
+  </div>
 </div>
