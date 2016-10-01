6bb161e9281117c5fd95426e8dad1856d7e25def
Shen Yichen <2007.yichen@gmail.com>
2016-10-01 17:27:39 +0800

    Validate on signup
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- application/view/login/register.php
+++ application/view/login/register.php
@@ -19,23 +19,23 @@
               <div class="input-field col l10 offset-l1 m12">
                 <i class="material-icons prefix">account_circle</i>
                 <input id="name" name="name" type="text" class="validate"
-                       value="<?php echo isset($name) ? $name : ''; ?>">
+                       value="<?php echo isset($name) ? $name : ''; ?>" required>
                 <label for="name">Name</label>
               </div>
               <div class="input-field col l10 offset-l1 m12">
                 <i class="material-icons prefix">email</i>
                 <input id="email" name="email" type="email" class="validate"
-                       value="<?php echo isset($email) ? $email : ''; ?>">
+                       value="<?php echo isset($email) ? $email : ''; ?>" required>
                 <label for="email">Email</label>
               </div>
               <div class="input-field col l5 offset-l1 m12">
                 <i class="material-icons prefix">lock</i>
-                <input id="password" name="password" type="password" class="validate">
+                <input id="password" name="password" type="password" class="validate" required minlength="8">
                 <label for="password">Password</label>
               </div>
               <div class="input-field col l5 m12">
                 <i class="material-icons prefix">lock_outline</i>
-                <input id="password_confirm" name="password_confirm" type="password" class="validate">
+                <input id="password_confirm" name="password_confirm" type="password" class="validate" required minlength="8">
                 <label for="password_confirm">Password Confirmation</label>
               </div>
             </div>
