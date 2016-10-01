47b5acc78a6f1fe2e5d09e8dcd6e043356fd1a22
Shen Yichen <2007.yichen@gmail.com>
2016-10-01 17:42:02 +0800

    Redirect to landing page
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- application/Controller/HomeController.php
+++ application/Controller/HomeController.php
@@ -2,11 +2,16 @@
 
 namespace Mini\Controller;
 
-class HomeController
+class HomeController extends ApplicationController
 {
   public function index()
   {
-    //TODO add a landing page
-    header('Location: ' . URL . 'projects');
+    if($this->User->loggedIn()){
+      header('Location: ' . URL . 'projects');
+    } else {
+      include APP . 'view/_templates/header.php';
+      include APP . 'view/home/index.php';
+      include APP . 'view/_templates/footer.php';
+    }
   }
 }
