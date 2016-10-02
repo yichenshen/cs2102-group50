84b456ff683de3147e898e3a3803b2fd9f7a22a9
Shen Yichen <2007.yichen@gmail.com>
2016-10-02 20:53:18 +0800

    Add total amount
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- application/Model/Funding.php
+++ application/Model/Funding.php
@@ -16,4 +16,14 @@ class Funding extends Model
     $query->execute($parameters);
     return $query->fetchAll();
   }
+
+  public function totalAmount($project_id)
+  {
+    $sql = "SELECT sum(amount) AS total_amount FROM fundings WHERE project_id = :pid";
+    $query = $this->db->prepare($sql);
+    $parameters = array(':pid' => $project_id);
+
+    $query->execute($parameters);
+    return $query->fetch();
+  }
 }
