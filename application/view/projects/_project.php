344cbbdc56a116e57fa347c0fb22b4f11f0ad6f4
Shen Yichen <2007.yichen@gmail.com>
2016-10-02 20:38:54 +0800

    Funding button
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- application/view/projects/_project.php
+++ application/view/projects/_project.php
@@ -6,6 +6,9 @@
   <td><?php echo htmlspecialchars($project->amount) ?></td>
   <td>
     <form action="/projects/delete/<?php echo $project->id ?>" method="post">
+      <a class="btn-floating amber accent-3" href="/fundings/<?php echo $project->id ?>">
+        <i class="material-icons">attach_money</i>
+      </a>
       <a class="btn-floating light-green" href="/projects/edit/<?php echo $project->id ?>">
         <i class="material-icons">edit</i>
       </a>
