9f36a46f58552993063ff45ff3c11fbeeaf44d5c
Shen Yichen <2007.yichen@gmail.com>
2016-10-11 18:33:05 +0800

    Add admin controller
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

---
+++ application/Controller/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mini\Controller;
+
+class AdminController
+{
+
+  public function index()
+  {
+    if (!$this->User->loggedIn()) {
+      header('Location: ' . URL . 'login');
+    }
+
+    require APP . 'view/_templates/header.php';
+    require APP . 'view/admin/index.php';
+    require APP . 'view/_templates/footer.php';
+  }
+}
