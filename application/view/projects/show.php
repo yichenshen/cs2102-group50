d7e5340901f9c55d159451eb375532f2eb12b3ae
Shen Yichen <2007.yichen@gmail.com>
2016-10-12 21:38:36 +0800

    Restrict contribution to logged in users
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- application/view/projects/show.php
+++ application/view/projects/show.php
@@ -1,6 +1,7 @@
 <div class="container">
 
-  <img class="materialboxed centered-image" src="<?php echo '/' . (!($project->display_image) ? 'img/filler.jpg' : $project->display_image) ?>">
+  <img class="materialboxed centered-image"
+       src="<?php echo '/' . (!($project->display_image) ? 'img/filler.jpg' : $project->display_image) ?>">
 
   <h1><?php echo $project->title ?></h1>
 
@@ -12,6 +13,16 @@
 
   <div class="divider"></div>
 
-  <h3>Contribute!</h3>
-  <?php include APP . 'view/fundings/_form.php' ?>
+  <?php if ($this->User->loggedIn()) { ?>
+    <h3>Contribute!</h3>
+    <?php include APP . 'view/fundings/_form.php' ?>
+  <?php } else { ?>
+    <br />
+    <div class="center">
+      <a href="/login"
+         class="btn-large waves-effect waves-light secondary-accent">Log in to contribute!</a>
+    </div>
+    <br />
+  <?php } ?>
+
 </div>
