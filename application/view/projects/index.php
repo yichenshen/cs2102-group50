090a792538882324bd05527dc40818cef8299478
Shen Yichen <2007.yichen@gmail.com>
2016-10-12 21:41:59 +0800

    Remove index button if not logged in
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- application/view/projects/index.php
+++ application/view/projects/index.php
@@ -1,12 +1,14 @@
 <div class="container">
-  <a href="/projects/newproject" class="btn-floating btn-large light-green right">
-    <i class="material-icons">add</i>
-  </a>
+  <?php if ($this->User->loggedIn()) { ?>
+    <a href="/projects/newproject" class="btn-floating btn-large right secondary-accent">
+      <i class="material-icons">add</i>
+    </a>
+  <?php } ?>
   <h1>Projects</h1>
   <!-- main content output -->
   <div class="row">
-      <?php foreach ($projects as $project) {
-        include '_project.php';
-      } ?>
+    <?php foreach ($projects as $project) {
+      include '_project.php';
+    } ?>
   </div>
 </div>
