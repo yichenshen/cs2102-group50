d6f2142437181a02e8e957cce16e0110d19a2347
Shen Yichen <2007.yichen@gmail.com>
2016-10-26 23:04:30 +0800

    Add edit button
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- application/view/admin/index.php
+++ application/view/admin/index.php
@@ -1,5 +1,23 @@
 <div class="container">
-  <h1><?php echo $this->User->currentUserName() ?></h1>
+  <a class="btn btn-floating btn-large secondary-500 right" href="/admin/edit">
+    <i class="material-icons">edit</i>
+  </a>
+  <h1>
+    <?php echo $this->User->currentUserName() ?>
+    <?php if ($this->User->currentIsAdmin()) {
+      echo '(Admin)';
+    } ?>
+  </h1>
+
+  <p>
+    <h5>Email: <?php echo $this->User->currentUserEmail(); ?></h5>
+  </p>
+
+  <br/>
+  <br/>
+
+  <h4>My Projects</h4>
+  <div class="divider"></div>
 
   <?php foreach ($projects as $project) {
     include APP . 'view/projects/_project.php';
