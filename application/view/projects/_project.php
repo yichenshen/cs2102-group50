d39080a2df2f6ef4bc473b2b4518e2ec7353a1ea
Shen Yichen <2007.yichen@gmail.com>
2016-10-31 01:48:06 +0800

    Progress bar
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- application/view/projects/_project.php
+++ application/view/projects/_project.php
@@ -14,7 +14,18 @@
           <i class="material-icons right">more_vert</i>
         </a>
       </span>
-      <p class="truncate"><?php echo htmlspecialchars($project->description) ?></p>
+      <p>Ends: <?php echo htmlspecialchars($project->end_date) ?></p>
+      <?php
+      $progress = ($project->amount_raised ?: 0) / ($project->amount) * 100;
+      ?>
+
+      <div class="progress">
+        <div class="determinate primary-700" style="width: <?php echo $progress > 100 ? 100 : $progress; ?>%"></div>
+      </div>
+      <p>
+        $<?php echo($project->amount_raised ?: 0) ?>/$<?php echo $project->amount; ?>
+        (<?php echo $progress; ?>%)
+      </p>
     </div>
     <div class="card-reveal secondary-300">
       <span class="card-title grey-text text-darken-4">
@@ -22,7 +33,6 @@
         <i class="material-icons right">close</i></span>
       <p>From <?php echo htmlspecialchars($project->start_date) ?>
         to <?php echo htmlspecialchars($project->end_date) ?></p>
-      <p>Funding Sought: <?php echo htmlspecialchars($project->amount) ?> SGD</p>
       <p><?php echo nl2br(htmlspecialchars($project->description)) ?></p>
     </div>
   </div>
