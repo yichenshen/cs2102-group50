5c31fc1fc464dd6011ffb9967f394d1917b081d7
Shen Yichen <2007.yichen@gmail.com>
2016-11-07 14:29:05 +0800

    Seed admin account
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- _install/data.sql
+++ _install/data.sql
@@ -1,24 +1,2 @@
-/* Sample SQL queries (Data inserted into tables) */
-/*
-INSERT INTO users (email, name) VALUES ('abc@example.com', 'abc');
-INSERT INTO users (email, name) VALUES ('john_smith@gmail.com', 'John Smith');
-INSERT INTO users (email, name) VALUES ('blackr@yahoo.com', 'Rebacca Black');
-INSERT INTO users (email, name) VALUES ('huawei@huawei.com', 'Hua Wei');
-INSERT INTO users (email, name) VALUES ('ahmadzaki@hotmail.com', 'Ahmad Zaki');
-INSERT INTO users (email, name) VALUES ('lily_angel@nus.edu.com', 'Lily');
-INSERT INTO users (email, name) VALUES ('lily_devil@ntu.edu.com', 'Lily');
-INSERT INTO users (email, name) VALUES ('johnlittle@johnlittle.com', 'John Little');
-INSERT INTO users (email, name) VALUES ('stephan_bressan@comp.nus.edu.com', 'Stephan Bressan');
-INSERT INTO users (email, name) VALUES ('lmljanice@comp.nus.edu.com', 'Lee Mong Li Janice');
-
-
-INSERT INTO projects (owner, title, start_date, amount) VALUES
-('abc@example.com', 'test project', '2008-01-01', 100);
-INSERT INTO projects (owner, title, start_date, amount) VALUES
-('stephan_bressan@comp.nus.edu.com', 'Crowdfunding database', '2016-09-23', 10);
-
-INSERT INTO fundings (id, p_id, funder, amount) VALUES
-(DEFAULT, 1, 'huawei@huawei.com', 1000);
-INSERT INTO fundings (id, p_id, funder, amount) VALUES
-(DEFAULT, 2, 'lmljanice@comp.nus.edu.com', 550);
-*/
+INSERT INTO users (name, email, password, is_admin)
+VALUES ('Admin', 'admin@admin.com', '$2y$10$80.NLueLLUloHm9RG1dS/euy.O9PrFvRVABk1q/yeEIPY5/MODd8O', TRUE);
