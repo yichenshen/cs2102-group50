498b38c6462569a0ed384a758a1d6f3f8ab1d833
Shen Yichen <2007.yichen@gmail.com>
2016-11-09 17:04:08 +0800

    Cascade deletes
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- _install/schema.sql
+++ _install/schema.sql
@@ -16,20 +16,21 @@ VALUES ('Admin', 'admin@admin.com', '$2y$10$80.NLueLLUloHm9RG1dS/euy.O9PrFvRVABk
 
 CREATE TABLE projects (
   id            SERIAL PRIMARY KEY,
-  owner         VARCHAR(256) REFERENCES users (email) ON UPDATE CASCADE NOT NULL,
-  title         TEXT                                                    NOT NULL,
+  owner         VARCHAR(256) REFERENCES users (email) ON UPDATE CASCADE ON DELETE CASCADE  NOT NULL,
+  title         TEXT                                                                       NOT NULL,
   description   TEXT,
-  start_date    DATE                                                    NOT NULL,
-  end_date      DATE                                                    NOT NULL,
-  category      TEXT                                                    NOT NULL DEFAULT 'Others' CHECK (category IN
-                                                                                                         ('Art', 'Charity', 'Crafts', 'Design', 'Technology', 'Others')),
+  start_date    DATE                                                                       NOT NULL,
+  end_date      DATE                                                                       NOT NULL,
+  category      TEXT                                                                       NOT NULL DEFAULT 'Others' CHECK (
+    category IN
+    ('Art', 'Charity', 'Crafts', 'Design', 'Technology', 'Others')),
   display_image TEXT,
-  amount        FLOAT                                                   NOT NULL CHECK (amount > 0)
+  amount        FLOAT                                                                      NOT NULL CHECK (amount > 0)
 );
 
 CREATE TABLE fundings (
   id         SERIAL PRIMARY KEY,
-  project_id INT REFERENCES projects (id),
-  funder     VARCHAR(256) REFERENCES users (email) ON UPDATE CASCADE,
+  project_id INT REFERENCES projects (id) ON UPDATE CASCADE ON DELETE CASCADE,
+  funder     VARCHAR(256) REFERENCES users (email) ON UPDATE CASCADE ON DELETE CASCADE,
   amount     FLOAT NOT NULL CHECK (amount > 0)
 );
